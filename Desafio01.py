a1de22587b279adb7f62ab32d219add4fbc379c5
Julielle Santos <julielle.santos@gmail.com>
2025-08-21 18:42:23 -0300

    ajuste no calculo

--- Desafio01.py
+++ Desafio01.py
@@ -3,6 +3,6 @@ nome = (input("Digite seu nome:"))
 salario = float(input("Digite seu salario:"))
 perc_bonus = float(input("Percentual do bonus:"))
 
-valor_bonus = 1000 + salario * perc_bonus
+valor_bonus = ((1000 + salario) * perc_bonus)
 
 print(f"Olá, {nome}. Seu bonus é de: {valor_bonus}")
