e0101fc93e5190b9d84e5fc9e4d2214ae11b09da
Julielle Santos <julielle.santos@gmail.com>
2025-08-21 18:39:13 -0300

    Incluindo desafio 01

---
+++ Desafio01.py
@@ -0,0 +1,8 @@
+nome = (input("Digite seu nome:"))
+
+salario = float(input("Digite seu salario:"))
+perc_bonus = float(input("Percentual do bonus:"))
+
+valor_bonus = 1000 + salario * perc_bonus
+
+print(f"Olá, {nome}. Seu bonus é de: {valor_bonus}")
