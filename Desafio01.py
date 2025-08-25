80133aaa21b79c925b5de7cf0f85dcc6ae5d1f67
Julielle Santos <julielle.santos@gmail.com>
2025-08-25 14:25:05 -0300

    Inclusão de tratativa de erro e novos exercicios

--- Desafio01.py
+++ Desafio01.py
@@ -1,8 +1,63 @@
-nome = (input("Digite seu nome:"))
+nome = input("Digite seu nome:")
+while nome.isalpha() == False :
+    if nome.isdigit():
+        print(" -Você digitou algo numero")
+        
+    elif len(nome) == 0:
+        print(" -Você não digitou nada")
+        
+    elif nome.isspace():
+        print(" -Espaço não é validado")
+    nome = input("Digite seu nome:")
+        
+print("-----------------------")
 
-salario = float(input("Digite seu salario:"))
-perc_bonus = float(input("Percentual do bonus:"))
+while True:
+    salario = input("Digite seu salario:")
 
-valor_bonus = ((1000 + salario) * perc_bonus)
+    if salario.isalpha():
+        print(" -Digite apenas numeros. Ex 899.00 ou 899,00")
 
+    elif len(salario) == 0:
+        print(" -Você não digitou nada")
+    
+    elif salario.isspace():
+        print(" -Espaço não é validado")
+    
+    else:
+        try:
+            salario = float(salario.replace(",", "."))
+            break
+        except ValueError:
+            print(" Valor invalido. Digite por exemplo 998.00 ou 998,00")
+
+    
+print("-----------------------")
+
+while True :
+
+    perc_bonus = input("Percentual do bonus:")
+
+    
+    if perc_bonus.isalpha():
+        print(" -Digite apenas numeros. Ex 1,5 ou 1.5")
+
+    elif len(perc_bonus) == 0:
+        print(" -Você não digitou nada")
+    
+    elif perc_bonus.isspace():
+        print(" -Espaço não é validado")
+    else:
+        try:
+            perc_bonus = float(perc_bonus.replace(",", "."))
+            break
+        except ValueError:
+            print(" Valor invalido. Digite por exemplo 1.0 ou 1,0")
+    
+
+valor_bonus = ((1000 + salario) * (perc_bonus / 100))
+recebimento = salario + valor_bonus
+print("-----------------------")
 print(f"Olá, {nome}. Seu bonus é de: {valor_bonus}")
+print(f"Esse mês irá receber: R$ {recebimento}")
+exit()
